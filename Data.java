782de00b69ccee108ab71037b940bf8ca19cfebc
julliet <71382135+tispen@users.noreply.github.com>
2023-06-14 13:54:16 +0300

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class Data {
+    String name;
+    String prog;
+
+    public Data(String name, String prog) {
+        this.name = name;
+        this.prog = prog;
+    }
+}
+
